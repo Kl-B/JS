57aabee3eba5745a785ada35005965a78b59cf58
Klara Bergman <89419634+Kl-B@users.noreply.github.com>
2022-01-26 10:13:34 +0100

    lektion 2

--- script.js
+++ script.js
@@ -35,3 +35,37 @@ function mathAdd() {
   let number = document.getElementById("math").value;
   document.getElementById("displaytext3").innerText = parseFloat(number) + 300;
 }
+
+function mathAddTwo() {
+  let input1 = document.getElementById("firstnumber").value;
+  let input2 = document.getElementById("secondnumber").value;
+
+  document.getElementById("displaytext4").innerText =
+    parseFloat(input1) + parseFloat(input2);
+}
+
+function mathCompare() {
+  let input1 = document.getElementById("compare1").value;
+  let input2 = document.getElementById("compare2").value;
+
+  if (input1 > input2) {
+    document.getElementById("displaytext5").innerText = "YES";
+  } else if (input1 < input2) {
+    document.getElementById("displaytext5").innerText = "NO";
+  } else {
+    document.getElementById("displaytext5").innerText = "EQUAL";
+  }
+}
+
+let number1 = 1;
+function addOne() {
+  number1++;
+  document.getElementById("displaytext6").innerText = number1;
+}
+
+let isOn = false;
+function toggle() {
+  isOn = !isOn;
+  document.getElementById("displaytext7").innerText =
+    isOn === true ? "On" : "Off";
+}
